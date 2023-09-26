c5382e342c77a8e0095aaff9d77dd5bf7794a0ed
Colin <ctberry21@gmail.com>
2023-09-26 18:23:20 -0500

    Implemented Multiplication Feature

--- calculator.c
+++ calculator.c
@@ -2,14 +2,16 @@
 
 int addition(int a, int b);
 int subtraction(int a, int b);
+int multiplication(int a, int b);
 
 int main () {
 
 	int a = 6;
 	int b = 3;
 
-	printf("%d",addition(a, b));
-	printf("%d",subtraction(a, b));
+	printf("%d\n",addition(a, b));
+	printf("%d\n",subtraction(a, b));
+	printf("%d\n", multiplication(a, b));
 
 	return 0;
 }
@@ -20,4 +22,8 @@ int addition(int a, int b){
 
 int subtraction(int a, int b){
 	return a - b;
+}
+
+int multiplication(int a, int b){
+	return a*b;
 }
