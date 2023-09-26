6b28d6e54df20b5d7928236a487f0fb12b6228e9
Colin <ctberry21@gmail.com>
2023-09-26 18:41:59 -0500

    Added user interface for calculator operations

--- calculator.c
+++ calculator.c
@@ -3,17 +3,33 @@
 int addition(int a, int b);
 int subtraction(int a, int b);
 int multiplication(int a, int b);
-double division(int a, int b);
+double division(double a, double b);
 
 int main () {
 
-	int a = 6;
-	int b = 3;
+	int a;
+	int b;
+	int selection;
 
-	printf("%d\n",addition(a, b));
-	printf("%d\n",subtraction(a, b));
-	printf("%d\n", multiplication(a, b));
-	printf("%lf\n", division(a, b));	
+	printf("Select an operation: Addition = 1; Subtraction = 2; Multiplication = 3; Division = 4\n");
+	scanf("%d", &selection);
+	printf("Enter two whole numbers: ");
+	scanf("%d %d", &a, &b);
+	
+	switch (selection) {
+		case 1:
+			printf("%d\n",addition(a, b));
+			break;
+		case 2:
+			printf("%d\n",subtraction(a, b));
+			break;
+		case 3:
+			printf("%d\n", multiplication(a, b));
+			break;
+		case 4:
+			printf("%lf\n", division(a, b));
+			break;
+	}	
 
 	return 0;
 }
@@ -30,7 +46,7 @@ int multiplication(int a, int b){
 	return a*b;
 }
 
-double division(int a, int b){
+double division(double a, double b){
 	if (b!=0)
 		return a / b;
 	else
