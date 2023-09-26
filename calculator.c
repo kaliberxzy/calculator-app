ff7ad24755361ed38d014cfd7600ef4c8df6a8a6
Colin <ctberry21@gmail.com>
2023-09-26 18:15:22 -0500

    Implemented Addition Function

--- calculator.c
+++ calculator.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int addition(int a, int b); 
+
 int main () {
-	printf("Hello World");
+
+	int a = 6;
+	int b = 3;
+
+	printf("%d",addition(a, b));
+	
 	return 0;
+}
+
+int addition(int a, int b){
+	return a + b;
 }
