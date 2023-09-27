1bc6d817f1653c23a61a299b8f353aa615e89f16
Colin <ctberry21@gmail.com>
2023-09-26 19:12:30 -0500

    Added comments and documentation

--- calculator.c
+++ calculator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Function Prototypes for Math Operations
 int addition(int a, int b);
 int subtraction(int a, int b);
 int multiplication(int a, int b);
@@ -7,25 +8,29 @@ double division(double a, double b);
 
 int main() {
 
+	// Declares variables for user's desired operation and their numbers entered
     int a;
     int b;
     int selection;
 
+    // Prompts user to input their selection and stores the value in selection variable
     printf("Select an operation: Addition = 1; Subtraction = 2; Multiplication = 3; Division = 4\n");
     scanf("%d", &selection);
     
+    // Input validation - if selection is not 1 through 4, output an error and end program
     if (selection < 1 || selection > 4) {
         printf("Invalid operation selection.\n");
         return 1;
     }
 
+    // Prompts user to input two numbers. If they are not in the format descibed in scanf, output an error and end program
     printf("Enter two whole numbers: ");
-    
     if (scanf("%d %d", &a, &b) != 2) {
         printf("Invalid input.\n");
         return 1; 
     }
 
+    // Switch statements used to output value of function call based on user's selection value
     switch (selection) {
         case 1:
             printf("%d\n", addition(a, b));
@@ -37,6 +42,7 @@ int main() {
             printf("%d\n", multiplication(a, b));
             break;
         case 4:
+        	// If statement used to prevent division by zero - i.e. function is called only if b is not 0. 
             if (b != 0) {
                 printf("%lf\n", division(a, b));
             } else {
@@ -49,18 +55,22 @@ int main() {
     return 0;
 }
 
+// Function that adds two integers
 int addition(int a, int b) {
     return a + b;
 }
 
+// Function that subtarcts two integers
 int subtraction(int a, int b) {
     return a - b;
 }
 
+// Function that multiplys two integers 
 int multiplication(int a, int b) {
     return a * b;
 }
 
+// Function that divides two integers (implicit type conversion) and returns a double
 double division(double a, double b) {
     return a / b;
 }
