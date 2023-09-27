6c65d9270726f6e89d953ab51d6998240955eed9
Colin <ctberry21@gmail.com>
2023-09-26 19:02:58 -0500

    Implemented error handling for invalid inputs

--- calculator.c
+++ calculator.c
@@ -5,50 +5,62 @@ int subtraction(int a, int b);
 int multiplication(int a, int b);
 double division(double a, double b);
 
-int main () {
+int main() {
 
-	int a;
-	int b;
-	int selection;
+    int a;
+    int b;
+    int selection;
 
-	printf("Select an operation: Addition = 1; Subtraction = 2; Multiplication = 3; Division = 4\n");
-	scanf("%d", &selection);
-	printf("Enter two whole numbers: ");
-	scanf("%d %d", &a, &b);
-	
-	switch (selection) {
-		case 1:
-			printf("%d\n",addition(a, b));
-			break;
-		case 2:
-			printf("%d\n",subtraction(a, b));
-			break;
-		case 3:
-			printf("%d\n", multiplication(a, b));
-			break;
-		case 4:
-			printf("%lf\n", division(a, b));
-			break;
-	}	
+    printf("Select an operation: Addition = 1; Subtraction = 2; Multiplication = 3; Division = 4\n");
+    scanf("%d", &selection);
+    
+    if (selection < 1 || selection > 4) {
+        printf("Invalid operation selection.\n");
+        return 1;
+    }
 
-	return 0;
+    printf("Enter two whole numbers: ");
+    
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("Invalid input.\n");
+        return 1; 
+    }
+
+    switch (selection) {
+        case 1:
+            printf("%d\n", addition(a, b));
+            break;
+        case 2:
+            printf("%d\n", subtraction(a, b));
+            break;
+        case 3:
+            printf("%d\n", multiplication(a, b));
+            break;
+        case 4:
+            if (b != 0) {
+                printf("%lf\n", division(a, b));
+            } else {
+                printf("You cannot divide by ZERO!!!!!\n");
+                return 1; 
+            }
+            break;
+    }
+
+    return 0;
 }
 
-int addition(int a, int b){
-	return a + b;
+int addition(int a, int b) {
+    return a + b;
 }
 
-int subtraction(int a, int b){
-	return a - b;
+int subtraction(int a, int b) {
+    return a - b;
 }
 
-int multiplication(int a, int b){
-	return a*b;
+int multiplication(int a, int b) {
+    return a * b;
 }
 
-double division(double a, double b){
-	if (b!=0)
-		return a / b;
-	else
-		return -1;
+double division(double a, double b) {
+    return a / b;
 }
